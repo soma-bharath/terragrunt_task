2ecda138786b3449132c2aaf5b0c46aba5103d96
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-23 22:59:15 +0530

    Create variables.tf

---
+++ infrastructure/modules/network/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  description = "The ID of the VPC where subnets will be created"
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
