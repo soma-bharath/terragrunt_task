c819754ebe8a63b9039e7df06cb9ef59d5898e47
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-23 23:07:12 +0530

    Create subnet.tf

---
+++ infrastructure/modules/network/subnet.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id            = var.vpc_id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id            = var.vpc_id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
