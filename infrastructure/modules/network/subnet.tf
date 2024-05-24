c67f8165cb940067114f7ba3268a6247908ff516
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-24 18:02:16 +0530

    Update subnet.tf

--- infrastructure/modules/network/subnet.tf
+++ infrastructure/modules/network/subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "public" {
   vpc_id            = var.vpc_id
   cidr_block        = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
+  availability_zone = element(var.availability_zones, count.index % length(var.availability_zones))
   tags = {
     Name = "public-subnet-${count.index + 1}"
   }
@@ -13,7 +13,7 @@ resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
   vpc_id            = var.vpc_id
   cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
+  availability_zone = element(var.availability_zones, count.index % length(var.availability_zones))
   tags = {
     Name = "private-subnet-${count.index + 1}"
   }
