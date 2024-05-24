7ce0b18bde29268181ee632216065f628f7d1f66
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-05-24 18:01:07 +0530

    Update variables.tf

--- infrastructure/modules/network/variables.tf
+++ infrastructure/modules/network/variables.tf
@@ -17,3 +17,8 @@ variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
 }
+
+variable "availability_zones" {
+  description = "List of availability zones to distribute subnets across"
+  type        = list(string)
+}
