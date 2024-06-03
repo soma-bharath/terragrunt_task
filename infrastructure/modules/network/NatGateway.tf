debfdce34cfcc9f597ccd407ba5fee8c9ff00867
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 19:00:29 +0530

    Update NatGateway.tf

--- infrastructure/modules/network/NatGateway.tf
+++ infrastructure/modules/network/NatGateway.tf
@@ -1,8 +1,23 @@
-resource "aws_nat_gateway" "my_nat_gateway" {
-  allocation_id = aws_eip.my_eip.id
+resource "aws_nat_gateway" "nategateway_a" {
+  allocation_id = aws_eip.Public_eip_a.id
   subnet_id     = aws_subnet.public_subnet_1.id
   tags = {
-    Name = "NATGateway"
+    Name = "NatGateway_A"
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
+    Date = local.current_date
+  }
+}
+
+resource "aws_nat_gateway" "nategateway_c" {
+  allocation_id = aws_eip.Public_eip_c.id
+  subnet_id     = aws_subnet.public_subnet_3.id
+  tags = {
+    Name = "NatGateway_C"
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
   }
 }
