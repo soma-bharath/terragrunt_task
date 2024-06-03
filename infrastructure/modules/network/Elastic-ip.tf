4e4803ff81d0447ccf56c607c4d27c642ecc3217
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 18:59:50 +0530

    Update Elastic-ip.tf

--- infrastructure/modules/network/Elastic-ip.tf
+++ infrastructure/modules/network/Elastic-ip.tf
@@ -1,7 +1,21 @@
-resource "aws_eip" "my_eip" {
+resource "aws_eip" "Public_eip_a" {
   vpc = true
   tags = {
-    Name = "My-EIP"
+    Name = "EIP_public_subnet_a
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
+    Date = local.current_date
+  }
+}
+
+resource "aws_eip" "Public_eip_c" {
+  vpc = true
+  tags = {
+    Name = "EIP_public_subnet_c
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
   }
 }
