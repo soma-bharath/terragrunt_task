b1d2b0ff56646ddc4eb36fcb40818a5a79d63442
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 23:00:32 +0530

    Update NatGateway.tf

--- infrastructure/modules/network/NatGateway.tf
+++ infrastructure/modules/network/NatGateway.tf
@@ -2,7 +2,7 @@ resource "aws_nat_gateway" "nategateway_a" {
   allocation_id = aws_eip.Public_eip_a.id
   subnet_id     = aws_subnet.public[0].id
   tags = {
-    Name = "NatGateway_A"
+    Name = "Space_prod_NatGateway_A"
     Environment  = var.env
     Email   = var.Email
     Project_Name = var.Project_Name
@@ -14,7 +14,7 @@ resource "aws_nat_gateway" "nategateway_c" {
   allocation_id = aws_eip.Public_eip_c.id
   subnet_id     = aws_subnet.public[2].id
   tags = {
-    Name = "NatGateway_C"
+    Name = "Space_prod_NatGateway_C"
     Environment  = var.env
     Email   = var.Email
     Project_Name = var.Project_Name
