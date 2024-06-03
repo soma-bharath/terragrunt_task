0a0afabbdd0d3e07ea44d2a6e42eb7b40ea8c76e
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 23:00:51 +0530

    Update internetgateway.tf

--- infrastructure/modules/network/internetgateway.tf
+++ infrastructure/modules/network/internetgateway.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "my_igw" {
   vpc_id = var.vpc_id
 
   tags = {
-    Name = var.Name
+    Name = "Space_prod_IG"
     Environment  = var.env
     Email   = var.Email
     Project_Name = var.Project_Name
